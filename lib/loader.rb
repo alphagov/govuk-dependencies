20660f037ce15f56cb15482f4bcd5ff5765171f0
Daniel Burnley <dan@danops.uk>
2018-01-24 17:25:27 +0000

    Group pull requests by application name

--- lib/loader.rb
+++ lib/loader.rb
@@ -2,3 +2,4 @@ require_relative './domain/pull_request'
 
 require_relative './use_cases/fetch_pull_requests'
 require_relative './gateways/pull_request'
+require_relative './presenters/pull_requests_by_application'
