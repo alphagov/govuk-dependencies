a2b10efbfaa66f335435ad3a8522cc67159698dd
Daniel Burnley <dan@danops.uk>
2018-01-25 11:16:15 +0000

    Refactor lets out of tests

--- spec/gateways/pull_request_spec.rb
+++ spec/gateways/pull_request_spec.rb
@@ -1,29 +1,30 @@
 describe Gateways::PullRequest do
-  let(:result) { described_class.new.execute }
-  let(:headers) do
-    { 'Content-Type' => 'application/json' }
-  end
-
-  let(:github_url) { 'https://api.github.com/search/issues?q=is:pr+user:alphagov+state:open+author:app/dependabot' }
-
   context 'No open pull requests from Dependabot' do
     before do
-      stub_request(:get, github_url)
-        .to_return(body: '{ "total_count": 0, "incomplete_results": false, "items": [] }', headers: headers)
+      stub_request(:get, 'https://api.github.com/search/issues?q=is:pr+user:alphagov+state:open+author:app/dependabot')
+        .to_return(
+          body: '{ "total_count": 0, "incomplete_results": false, "items": [] }',
+          headers: { 'Content-Type' => 'application/json' }
+        )
     end
 
     it 'Returns an empty array' do
-      result = described_class.new.execute
-      expect(result).to be_empty
+      expect(described_class.new.execute).to be_empty
     end
   end
 
   context 'There are open pull requests from Dependabot' do
     before do
-      stub_request(:get, github_url)
-        .to_return(body: File.read('spec/fixtures/pull_requests.json'), headers: headers)
+      stub_request(:get, 'https://api.github.com/search/issues?q=is:pr+user:alphagov+state:open+author:app/dependabot')
+        .to_return(
+          body: File.read('spec/fixtures/pull_requests.json'),
+          headers: { 'Content-Type' => 'application/json' }
+        )
     end
+
     it 'Returns a list of pull requests' do
+      result = described_class.new.execute
+
       expect(result.count).to eq(3)
       expect(result[0].title).to eq('Bump gds-sso from 13.5.0 to 13.5.1')
       expect(result[0].application_name).to eq('publisher')
