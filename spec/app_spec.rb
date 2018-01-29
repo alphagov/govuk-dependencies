a864b26b927c693f7d54b04eae293b5eb1d081b7
Daniel Burnley <dan@danops.uk>
2018-01-29 10:21:22 +0000

    Add error message for if no pull requests are found

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -8,45 +8,89 @@ describe GovukDependencies do
   include Rack::Test::Methods
   def app() described_class end
 
-  before do
-    stub_request(:get, 'https://api.github.com/search/issues?q=is:pr+user:alphagov+state:open+author:app/dependabot')
-      .to_return(body: File.read('spec/fixtures/pull_requests.json'), headers: { 'Content-Type' => 'application/json' })
-  end
-
-  context 'Pull request by application' do
-    it 'should show both applications with the number of open pull requests' do
-      get '/'
-      expect(last_response).to be_ok
-      expect(last_response.body).to include('frontend (1)')
-      expect(last_response.body).to include('publisher (2)')
-    end
-  end
-
-  context 'Pull requests by gem' do
-    it 'should show both gems with the number applications with pull requests' do
-      get '/gem'
-      expect(last_response).to be_ok
-      expect(last_response.body).to include('gds-sso (2)')
-      expect(last_response.body).to include('gds-api-adapters (1)')
-    end
-  end
-
-  context 'Pull requests by team' do
+  context 'given open pull requests' do
     before do
-      stub_request(:get, "https://docs.publishing.service.gov.uk/apps.json")
-      .to_return(
-        body: File.read('spec/fixtures/multiple_teams_with_multiple_applications.json'),
-        headers: { 'Content-Type' => 'application/json' }
-      )
+      stub_request(:get, 'https://api.github.com/search/issues?q=is:pr+user:alphagov+state:open+author:app/dependabot')
+        .to_return(body: File.read('spec/fixtures/pull_requests.json'), headers: { 'Content-Type' => 'application/json' })
     end
 
-    it 'should show both teams with the number applications with pull requests' do
-      get '/team'
-      expect(last_response).to be_ok
-      expect(last_response.body).to include('#modelling-services')
-      expect(last_response.body).to include('publisher (2)')
-      expect(last_response.body).to include('#start-pages')
-      expect(last_response.body).to include('frontend (1)')
+    context 'Pull request by application' do
+      it 'should show both applications with the number of open pull requests' do
+        get '/'
+        expect(last_response).to be_ok
+        expect(last_response.body).to include('frontend (1)')
+        expect(last_response.body).to include('publisher (2)')
+      end
+    end
+
+    context 'Pull requests by gem' do
+      it 'should show both gems with the number applications with pull requests' do
+        get '/gem'
+        expect(last_response).to be_ok
+        expect(last_response.body).to include('gds-sso (2)')
+        expect(last_response.body).to include('gds-api-adapters (1)')
+      end
+    end
+
+    context 'Pull requests by team' do
+      before do
+        stub_request(:get, "https://docs.publishing.service.gov.uk/apps.json")
+          .to_return(
+            body: File.read('spec/fixtures/multiple_teams_with_multiple_applications.json'),
+            headers: { 'Content-Type' => 'application/json' }
+        )
+      end
+
+      it 'should show both teams with the number applications with pull requests' do
+        get '/team'
+        expect(last_response).to be_ok
+        expect(last_response.body).to include('#modelling-services')
+        expect(last_response.body).to include('publisher (2)')
+        expect(last_response.body).to include('#start-pages')
+        expect(last_response.body).to include('frontend (1)')
+      end
+    end
+  end
+
+  context 'given no open pull requests' do
+    before do
+      stub_request(:get, 'https://api.github.com/search/issues?q=is:pr+user:alphagov+state:open+author:app/dependabot')
+        .to_return(
+          body: '{ "total_count": 0, "incomplete_results": false, "items": [] }',
+          headers: { 'Content-Type' => 'application/json' }
+        )
+    end
+
+    context 'Pull requests by application' do
+      it 'should display the "no open pull requests" message' do
+        get '/'
+        expect(last_response).to be_ok
+        expect(last_response.body).to include('No open pull requests found')
+      end
+    end
+
+    context 'Pull requests by gem' do
+      it 'should display the "no open pull requests" message' do
+        get '/gem'
+        expect(last_response).to be_ok
+        expect(last_response.body).to include('No open pull requests found')
+      end
+    end
+
+    context 'Pull requests by team' do
+      before do
+        stub_request(:get, "https://docs.publishing.service.gov.uk/apps.json")
+          .to_return(
+            body: File.read('spec/fixtures/multiple_teams_with_multiple_applications.json'),
+            headers: { 'Content-Type' => 'application/json' }
+        )
+      end
+
+      it 'should display the "no open pull requests" message' do
+        get '/team'
+        expect(last_response).to be_ok
+        expect(last_response.body).to include('No open pull requests found')
+      end
     end
   end
 end
