7183ccd77ad7a87137aaf6decb4ed37c075506e3
Daniel Burnley <dan@danops.uk>
2018-02-01 16:47:16 +0000

    Order items on application view

--- lib/presenters/pull_requests_by_application.rb
+++ lib/presenters/pull_requests_by_application.rb
@@ -5,13 +5,14 @@ module Presenters
         value.fetch(:application_name)
       end
 
-      grouped_pull_requests.map do |application_name, pull_requests|
+      pull_requests_by_application = grouped_pull_requests.map do |application_name, pull_requests|
         {
           application_name: application_name,
           application_url: application_url(application_name),
-          pull_requests: pull_requests
+          pull_requests: pull_requests.sort_by { |pr| pr[:title] }
         }
       end
+      pull_requests_by_application.sort_by { |app| app[:application_name] }
     end
 
   private
