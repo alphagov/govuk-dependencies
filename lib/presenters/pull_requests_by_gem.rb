9c67ee88ad17774d2ba2df9a6e2b041db71c7326
Daniel Burnley <dan@danops.uk>
2018-02-01 16:56:39 +0000

    Order items on gem view

--- lib/presenters/pull_requests_by_gem.rb
+++ lib/presenters/pull_requests_by_gem.rb
@@ -2,12 +2,13 @@ module Presenters
   class PullRequestsByGem
     def execute(ungrouped_pull_requests)
       grouped_pull_requests = ungrouped_pull_requests.group_by { |pr| gem_name(pr.fetch(:title)) }
-      grouped_pull_requests.map do |gem_name, pull_requests|
+      pull_requests_by_gem = grouped_pull_requests.map do |gem_name, pull_requests|
         {
           gem_name: gem_name,
-          pull_requests: pull_requests
+          pull_requests: pull_requests.sort_by { |pr| pr[:application_name] }
         }
       end
+      pull_requests_by_gem.sort_by { |gem| gem[:gem_name] }
     end
 
   private
