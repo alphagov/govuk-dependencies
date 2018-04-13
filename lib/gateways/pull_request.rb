72f34f5379acd847d3b6730a079c7e77c5e05a51
Isabell (Issy) Long <isabell@issyl0.co.uk>
2018-04-13 16:55:14 +0100

    Add a 'common filters' method to PR review state code
    - This prevents having to change the parameters that don't change very
      often in three places should they ever change.

--- lib/gateways/pull_request.rb
+++ lib/gateways/pull_request.rb
@@ -12,18 +12,22 @@ module Gateways
 
   private
 
+    def issue_search_common_filters
+      'is:pr user:alphagov state:open author:app/dependabot'
+    end
+
     def approved_pull_requests
-      approved_pull_requests = @octokit.search_issues('is:pr user:alphagov state:open author:app/dependabot review:approved').items
+      approved_pull_requests = @octokit.search_issues("#{issue_search_common_filters} review:approved").items
       build_pull_requests(approved_pull_requests, 'approved')
     end
 
     def review_required_pull_requests
-      review_required_pull_requests = @octokit.search_issues('is:pr user:alphagov state:open author:app/dependabot review:required').items
+      review_required_pull_requests = @octokit.search_issues("#{issue_search_common_filters} review:required").items
       build_pull_requests(review_required_pull_requests, 'review required')
     end
 
     def changes_requested_pull_requests
-      changes_requested_pull_requests = @octokit.search_issues('is:pr user:alphagov state:open author:app/dependabot review:changes_requested').items
+      changes_requested_pull_requests = @octokit.search_issues("#{issue_search_common_filters} review:changes_requested").items
       build_pull_requests(changes_requested_pull_requests, 'changes requested')
     end
 
