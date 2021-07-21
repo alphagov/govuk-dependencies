ba2a18b11fa0236d9a5a2ad1a2beb9a017ebd7f9
Kevin Dew <kevindew@me.com>
2021-07-21 18:43:07 +0100

    Auto-correct rubocop-govuk 4.0.0 offences
    rubocop -A

--- helpers/cache.rb
+++ helpers/cache.rb
@@ -1,7 +1,7 @@
-def cache(name, cache_duration_seconds = 120)
+def cache(name, cache_duration_seconds = 120, &block)
   return yield unless ENV["RACK_ENV"] == "production"
 
   UseCases::Cache.new(
     path: "#{Dir.pwd}/public/cache/#{name}.html",
-  ).execute(cache_duration_seconds: cache_duration_seconds) { yield }
+  ).execute(cache_duration_seconds: cache_duration_seconds, &block)
 end
